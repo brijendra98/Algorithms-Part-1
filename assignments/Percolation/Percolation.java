3eb1a64dd7cae692c6890025be8fe316555f961e
Brijendra Nag <brijendra98@gmail.com>
2017-12-27 12:41:34 +0530

    Percolation done, stats left

--- assignments/Percolation/Percolation.java
+++ assignments/Percolation/Percolation.java
@@ -1,61 +1,103 @@
+import java.lang.*;
+import java.io.*;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 import edu.princeton.cs.algs4.QuickFindUF;
 
 public class Percolation {
-  private int n;
-  private int grid[][]; //0 is closed, 1 is open
-  private QuickFindUF QF;
+  private int size;
+  //false is closed, true is open
+  private boolean grid[][];
+  private QuickFindUF UF;
+  private QuickFindUF UF2;
+  private int top;
+  private int bottom;
+  private int numOpen;
 
-   public Percolation(int n) {              // create n-by-n grid, with all sites blocked
-     if(n<=0) {
-       throw java.lang.IllegalArgumentException();
-     }
-     this.n = n;
-     grid = new int[n+1][n+1];
-     //int counter = 1;
-     for(int i=1; i<=n; i++) {
-       for(int j=1; j<=n; j++) {
-         grid[i][j] = 0;
-         //grid[i][j] = counter;
-         //counter++;
-       }
-     }
-     QF = new QuickFindUF(n*n);
-   }
+  // create n-by-n grid, with all sites blocked
+  public Percolation(int n) {
+    if(n<=0) {
+      //throw java.lang.IllegalArgumentException();
+    }
+    size = n;
+    numOpen = 0;
+    grid = new boolean[n+1][n+1];
+    UF = new QuickFindUF(n*n + 2);
+    UF2 = new QuickFindUF(n*n + 1);
+    top = n*n;
+    bottom = n*n + 1;
+  }
 
-   public void open(int row, int col) {     // open site (row, col) if it is not open already
-     if(grid[row][col] == 0) {
-       grid[row][col] = 1;
-       if(row-1>=1 && grid[row-1][col]==1) {
-         QF.union(((row-1)*n + col), (((row-1)-1)*n + col));
-       }
-       if(row+1<=n && grid[row+1][col]==1) {
-         QF.union(((row-1)*n + col), (((row+1)-1)*n + col));
-       }
-       if(col-1>=1 && grid[row][col-1]==1) {
-         QF.union(((row-1)*n + col), ((row-1)*n + (col-1)));
-       }
-       if(col+1<=n && grid[row][col+1]==1) {
-         QF.union(((row-1)*n + col), ((row-1)*n + (col+1)));
-       }
-     }
-   }
-   public boolean isOpen(int row, int col) {// is site (row, col) open?
-     return grid[row][col] == 1;
-   }
-   public boolean isFull(int row, int col) {// is site (row, col) full?
+  // open site (row, col) if it is not open already
+  public void open(int row, int col) {
+    if(!isOpen(row, col)) {
+      numOpen++;
+      grid[row][col] = true;
+      if(row==1) {
+        UF.union(((row-1)*size + col), top);
+        UF2.union(((row-1)*size + col), top);
+      }
+      if(row==size) {
+        UF.union(((row-1)*size + col), bottom);
+      }
+      if(row-1>=1 && grid[row-1][col]) {
+        UF.union(((row-1)*size + col), (((row-1)-1)*size + col));
+        UF2.union(((row-1)*size + col), (((row-1)-1)*size + col));
+      }
+      if(row+1<=size && grid[row+1][col]) {
+        UF.union(((row-1)*size + col), (((row+1)-1)*size + col));
+        UF2.union(((row-1)*size + col), (((row+1)-1)*size + col));
+      }
+      if(col-1>=1 && grid[row][col-1]) {
+        UF.union(((row-1)*size + col), ((row-1)*size + (col-1)));
+        UF2.union(((row-1)*size + col), ((row-1)*size + (col-1)));
+      }
+      if(col+1<=size && grid[row][col+1]) {
+        UF.union(((row-1)*size + col), ((row-1)*size + (col+1)));
+        UF2.union(((row-1)*size + col), ((row-1)*size + (col+1)));
+      }
+    }
+  }
 
-   }
-   public int numberOfOpenSites() {         // number of open sites
-   }
-   public boolean percolates()              // does the system percolate?
+  // is site (row, col) open?
+  public boolean isOpen(int row, int col) {
+    return grid[row][col];
+  }
 
-   public static void main(String[] args)   // test client (optional)
+  // is site (row, col) full?
+  public boolean isFull(int row, int col) {
+    return UF2.connected(((row-1)*size + col), top);
+  }
+
+  // number of open sites
+  public int numberOfOpenSites() {
+    return numOpen;
+  }
+
+  // does the system percolate?
+  public boolean percolates() {
+    return UF.connected(top, bottom);
+  }
+
+  // test client (optional)
+  public static void main(String[] args) throws IOException {
+    // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    // Percolation myClass = new Percolation(5);
+    // System.out.println(myClass.numberOfOpenSites());
+    // System.out.println(myClass.percolates());
+    // myClass.open(1,1);
+    // System.out.println(myClass.numberOfOpenSites());
+    // System.out.println(myClass.percolates());
+    // myClass.open(1,3);
+    // System.out.println(myClass.numberOfOpenSites());
+    // System.out.println(myClass.percolates());
+    // myClass.open(1,2);
+    // System.out.println(myClass.numberOfOpenSites());
+    // System.out.println(myClass.percolates());
+    // myClass.open(2,2);
+    // myClass.open(3,2);
+    // myClass.open(5,2);
+    // myClass.open(4,2);
+    // System.out.println(myClass.numberOfOpenSites());
+    // System.out.println(myClass.percolates());
+  }
 }
-
-// (1,1) (1,2)....
-// (2,1)
-// .
-// .
-// .
-// (r-1)*n+c
